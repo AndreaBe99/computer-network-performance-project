6a3c25142a032cd8384f4c2e96446c60019c6dd0
Andrea Bernini <36330520+AndreaBe99@users.noreply.github.com>
2022-07-25 17:10:52 +0200

    Delete ping-script.py

--- ping-script.py
+++
@@ -1,6 +0,0 @@
-import os
-for i in range(56, 184, 64):
-    response = os.popen(f"ping h2 - s " + str(i) +" - D - w 10").read()
-    print("#############################################")
-    print(response)
-    print("#############################################")
