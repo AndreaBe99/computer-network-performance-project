9ab308a422ab45b3032a1fee564fa436bb2a8da1
Donato Stanco <68702404+dstanco98@users.noreply.github.com>
2022-07-25 18:04:23 +0200

    add new version of ping script

---
+++ ping.py
@@ -0,0 +1,28 @@
+from concurrent.futures import thread
+import subprocess
+import os
+import ipaddress
+import time
+from time import sleep
+from threading import Thread
+from unicodedata import name
+
+# first we get the current timestamp
+t_now = time.time()
+print("Timestamp: ", t_now)
+
+ip_h3 = ipaddress.ip_address('10.0.0.3')
+
+def ping_host():
+    with open(os.devnull, "wb") as limbo:
+        for n in range(1,10,2):
+            thread = Thread(target=ping_host, args=n)
+            thread.start()
+            thread.join()
+            while(t_now < t_now + 60):   
+                # if we want to ping only one ip at a time, we have to uese subprocess.call()
+                ping = subprocess.Popen(["ping", "-c", "1", "-s", "15000", ip_h3],
+                                        stdout=subprocess.PIPE, stderr=limbo).wait()
+                output = ping.communicate()
+        # wait 1 sec in between each thread
+        sleep(1)
